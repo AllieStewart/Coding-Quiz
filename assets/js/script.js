3b653bb6e81f196b434a2a548738a985d180b803
AllieStewart <megaxis1217@gmail.com>
2023-09-30 19:01:12 -0500

    added idea comments for .html, .css, and .js files, trying to formulate the page design

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Questions, maybe string arrays x5?
+
+// var value = document.getElementById("id");
+// var name = value.querySelector("selector");
+
+// object for score tally, initials?
+
+// Saves high score to local storage
+function saveHighScore(){
+
+}
+
+// function for replacing old questions with new ones?
+
+// add functions under this and button names (Submit, Start, Go Back, etc)
+// button.addEventListener('click', functions (event){
+//     event.preventDefault();
+// }
