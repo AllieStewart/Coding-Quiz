8147d1b2e7676eb19e2109a2475071b5223976ec
AllieStewart <megaxis1217@gmail.com>
2023-10-01 17:03:30 -0500

    had a bit of an oops with declaring .css files, now .html is cleaned up nicely alongside .css

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,25 +30,25 @@ var questions = [
 // Header of page variables
 var viewScores = document.getElementById("view-scores");
 var timeLeft = document.getElementById("time-left");
-var startButton = document.querySelector(".start-button");
+var startButton = document.querySelector("start-button");
 
 // Questions page variables
-var gameQuestions = document.getElementById(".game-questions");
+var gameQuestions = document.getElementById("game-questions");
 var answersList = gameQuestions.querySelector("ol");
 
 // End of quiz page variables
-var gameOver = document.getElementById(".game-over");
+var gameOver = document.getElementById("game-over");
 var finalScore = document.getElementById("final-score");
 var gameOverForm = document.getElementById("game-over-form");
 var initialsID = document.getElementById("initials");
 
 // View high scores page variables
-var showScores = document.getElementById(".show-scores");
-var goBackButton = document.querySelector(".go-back-button");
-var clearHSButton = document.querySelector(".clear-hs-button");
+var showScores = document.getElementById("show-scores");
+var goBackButton = document.querySelector("go-back-button");
+var clearHSButton = document.querySelector("clear-hs-button");
 
 // Underneath questions page, Correct!/Wrong! variables
-var answersCorrectWrong = document.getElementById(".answers-correct-wrong")
+var answersCorrectWrong = document.getElementById("answers-correct-wrong")
 var asnwerState = document.getElementById("answer-state");
 
 // Saves high score to local storage
