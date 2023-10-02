5dc164543b570ba7409d363790f07c2bc021d6d2
AllieStewart <megaxis1217@gmail.com>
2023-10-02 17:31:39 -0500

    fixed button issue and visibility issues in .html, added most functions to .js, still need to connect asnwering questions and totaling score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Header of page variables
+var codingQuizMain = document.getElementById("coding-quiz-title");
+
 var viewScores = document.getElementById("view-scores");
 var timeLeft = document.getElementById("time-left");
-var startButton = document.querySelector("start-button");
+var startButton = document.querySelector("#start-button");
 
 // Questions page variables
 var gameQuestions = document.getElementById("game-questions");
@@ -12,50 +14,60 @@ var gameOver = document.getElementById("game-over");
 var finalScore = document.getElementById("final-score");
 var gameOverForm = document.getElementById("game-over-form");
 var initialsID = document.getElementById("initials");
-var submitButton = document.querySelector("submit-button");
+var submitButton = document.querySelector("#submit-button");
 
 // View high scores page variables
 var showScores = document.getElementById("show-scores");
-var goBackButton = document.querySelector("go-back-button");
-var clearHSButton = document.querySelector("clear-hs-button");
+var goBackButton = document.querySelector("#go-back-button");
+var clearHSButton = document.querySelector("#clear-hs-button");
 
 // Underneath questions page, Correct!/Wrong! variables
 var answersCorrectWrong = document.getElementById("answers-correct-wrong");
 var answerState = document.getElementById("answer-state");
 
+// Hidden/Visible
+var hiddenPage = document.getElementById(".hidden");
+var visiblePage = document.getElementById(".visible");
+
+
 // Questions; array of objects for each question, answers, and correct # (index)
 var questions = [
     {
-        question: "?",
-        answers: ["yes", "no"],
+        question: "In what order is the JavaScipt file loaded?",
+        answers: ["After <head>", "Before </main>", "Before </body>", "After </html>"],
+        correct: 2,
+    },
+    {
+        question: "What operator do you use to compare the same value AND datatype?",
+        answers: ["===", "==", "=", ">"],
         correct: 0,
     },
     {
-        question: "?",
-        answers: ["yes", "no"],
+        question: "Which method puts a window on the screen with only one button?",
+        answers: ["prompt()", "alert()", "confirm()", "console.log()"],
+        correct: 1,
+    },
+    {
+        question: "In a Document Object Model (DOM), what element is after the Document?",
+        answers: ["Root Element", "Body Element", "Head Element", "Title Element"],
         correct: 0,
     },
     {
-        question: "?",
-        answers: ["yes", "no"],
-        correct: 0,
-    },
-    {
-        question: "?",
-        answers: ["yes", "no"],
-        correct: 0,
-    },
-    {
-        question: "?",
-        answers: ["yes", "no"],
-        correct: 0,
+        question: "Which method can remove an element from an array?",
+        answers: ["pop()", "shift()", "splice()", "All of the above."],
+        correct: 3,
     }
 ];
 
 // Other variables for function values (defaults)
-var timer = 0;
+var timer;
 var timerCount;
 var isCorrect = false;
+
+var userScore = {
+    initials: initialsID.value,
+    score: timeLeft.value
+};
 
 // Loads high scores from previous playthrough (if any)
 function init()
@@ -66,19 +78,22 @@ function init()
 // Starts the quiz
 function startQuiz()
 {
-    isWin = false;
+    isCorrect = false;
     timerCount = 70;
 
     startTimer();
+    toggleDisplay(codingQuizMain);
+    toggleDisplay(gameQuestions);
+    answerQuestion();
 }
 
-// Starts the timer for the quiz
+// Starts the timer for startQuiz
 function startTimer()
 {
     // Edit functionality
     timer = setInterval(function() {
         timerCount--;
-        timerElement.textContent = timerCount;
+        timeLeft.textContent = timerCount;
         if (timerCount >= 0) {
           // Tests if win condition is met
           if (isCorrect && timerCount > 0) {
@@ -102,7 +117,7 @@ function answerQuestion()
 
 }
 
-// For "Correct" or "Wrong"
+// For "Correct" or "Wrong" in answerQuestion
 function changeState()
 {
 
@@ -111,26 +126,47 @@ function changeState()
 // Ends the quiz
 function endQuiz()
 {
-
+    toggleDisplay(gameOver);
+    saveHighScore();
 }
 
 // Saves high score to local storage
 function saveHighScore()
 {
-    // needs to save initials + score from timer
-    // initialsID + timeLeft?
+    localStorage.setItem('userScore', JSON.stringify(userScore));
 }
 
 // Gets high scores from local storage [View High Scores]
 function getHighScores()
 {
     // needs to retrieve list of initials + scores
+    var getHS = JSON.parse(localStorage.getItem(userScore));
+
+    if(getHS !== null)
+    {
+        initialsID.innerHTML = getHS.initials;
+        timeLeft.innerHTML = getHS.score;
+    }
+
 }
 
 // Clears high scores from local storage
 function clearHighScores()
 {
-    // needs to clear high score
+    localStorage.clear();
+}
+
+// Toggles blocks of code display or hide
+function toggleDisplay(x)
+{
+    x.classList.toggle("hidden");
+//     if (hiddenPage.style.display === "none") {
+//        hiddenPage.style.display = "block";
+//     } else {
+//        hiddenPage.style.display = "none";
+//     }
+
+//     if (visiblePage.style.display === "")
 }
 
 // checkWin()?
