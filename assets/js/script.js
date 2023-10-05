55036e0b48d31ed4dcf3128d78f601d9e4f112df
AllieStewart <megaxis1217@gmail.com>
2023-10-04 20:24:40 -0500

    can't get initials to show up on the board, but most of everything else is fixed as best as can be, added placehold to .html input as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
-// Header of page variables
+// Header of page variables; the main quiz page
 var codingQuizMain = document.getElementById("coding-quiz-title");
 
-//View Scores on top left, Time left on top right, start button after Coding Quiz Challenge
+// 'View Scores' on top left, 'Time left' on top right, 'Start' button after 'Coding Quiz Challenge'
 var viewScores = document.getElementById("view-scores");
 var timeLeft = document.getElementById("time-left");
 var startButton = document.getElementById("start-button");
 
-// Questions page variables
+// Questions page variables, with spot for questions
 var gameQuestions = document.getElementById("game-questions");
 var questionsList = gameQuestions.querySelector("p");
-// Answers to choose from section
+// Answers to choose from section, with ordered list
 var answersList = gameQuestions.querySelector("ol");
 var firstChoice = document.getElementById("button1");
 var secondChoice = document.getElementById("button2");
 var thirdChoice = document.getElementById("button3");
 var fourthChoice = document.getElementById("button4");
 
-// End of quiz page variables
+// End of quiz page variables, with a 'Submit' button
 var gameOver = document.getElementById("game-over");
 var finalScore = document.getElementById("final-score");
-var gameOverForm = document.getElementById("game-over-form");
-var initialsID = document.getElementById("initials");
+//var gameOverForm = document.getElementById("game-over-form"); // Never used the form
+var initialsID = document.getElementById("initials").value;
 var submitButton = document.getElementById("submit-button");
 
-// View high scores page variables
+// View high scores page variables, with 'Go back' and 'Clear high scores' buttons
 var showScores = document.getElementById("show-scores");
 var goBackButton = document.getElementById("go-back-button");
 var clearHSButton = document.getElementById("clear-hs-button");
@@ -32,9 +32,10 @@ var clearHSButton = document.getElementById("clear-hs-button");
 var answersCorrectWrong = document.getElementById("answers-correct-wrong");
 var answerState = document.getElementById("answer-state");
 
-// Hidden/Visible
+// Hidden toggle for pages
 var hiddenPage = document.getElementById(".hidden");
-var visiblePage = document.getElementById(".visible");
+// Do not seem to have a use for the "visible" element
+// var visiblePage = document.getElementById(".visible");
 
 
 // Questions; array of objects for each question, answers, and correct answer
@@ -67,19 +68,24 @@ var questions = [
 ];
 
 // Other variables for function values (defaults)
+// 'timer' for the setInterval, 'timerCount' for actual countdown + ending score,
+// 'currQuestion' is index of "question" in "questions[]", 
+// 'selectedAnswer' is user's selected answer
 var timer;
 var timerCount = 0;
 var currQuestion = 0;
 var selectedAnswer = 0;
 
+// The user's 'initials' and 'score', as defaults
 var userScore = {
-    initials: initialsID.value = "",
+    initials: initialsID,
     score: timerCount.value = 0
 };
 
-// Starts the quiz
+// Starts the quiz, toggles main display to be off, toggles questions page(s)
 function startQuiz()
 {
+    // Timer counter set to 70 seconds
     timerCount = 70;
 
     startTimer();
@@ -88,17 +94,18 @@ function startQuiz()
     showQuestion();
 }
 
-// Starts the timer for startQuiz
+// Starts the timer for function 'startQuiz'
 function startTimer()
 {
+    // Timer has setInterval
     timer = setInterval(function() {
         timerCount--;
         timeLeft.textContent = timerCount;
         // If time is less than or equal to 0 seconds
         if (timerCount <= 0) {
-            // Clears interval and stops timer
+            // Stops timer
             clearInterval(timer);
-
+            // Ends quiz if questions are done before timer is 0
             if(currQuestion < questions.length - 1)
             {
                 endQuiz();
@@ -107,14 +114,14 @@ function startTimer()
     }, 1000);
 }
 
-
+// Displays the questions, toggles 'Correct!/Wrong!' box underneath
 function showQuestion()
 {
     nextQuestion();
     toggleDisplay(answersCorrectWrong);
 }
 
-// For changing questions
+// For putting new questions/choices on each page
 function nextQuestion()
 {
     questionsList.textContent = questions[currQuestion].question;
@@ -126,7 +133,7 @@ function nextQuestion()
 
 }
 
-// For "Correct" or "Wrong", change to next question
+// For 'Correct!' or 'Wrong!', change to next question
 function changeState(selectedAnswer)
 {
     if(questions[currQuestion].correct === questions[currQuestion].answers[selectedAnswer])
@@ -137,44 +144,50 @@ function changeState(selectedAnswer)
     else
     {
         timerCount -= 10;
-        timerCount.textContent = timerCount;
+        timeLeft.textContent = timerCount;
         answerState.textContent="Wrong!";
     }
 
+    // Increment after answer is selected
     currQuestion++;
-    
+    // Changes page as long as there are still questions
     if (currQuestion < questions.length)
     {
         nextQuestion();
     }
-
+    // Ends quiz once questions run out
     else
     {
         endQuiz();
     }
 }
 
+// If user chooses first choice
 function chooseFirst() 
 { 
     changeState(0); 
 }
 
+// If user chooses second choice
 function chooseSecond() 
 { 
     changeState(1); 
 }
 
+// If user chooses third choice
 function chooseThird() 
 { 
     changeState(2); 
 }
 
+// If user chooses fourth choice
 function chooseFourth() 
 { 
     changeState(3); 
 }
 
-// Ends the quiz
+// Ends the quiz, toggles display for game over screen, removes questions,
+// sets the timer's counter to the user's score and displays it on screen
 function endQuiz()
 {
     toggleDisplay(gameOver);
@@ -188,50 +201,57 @@ function endQuiz()
 function saveHighScore(event)
 {
     event.preventDefault();
+
+    toggleDisplay(gameOver);
+    // Cannot enter empty space for initials
     if (initialsID.value === "") {
         alert("Enter your initials.");
         return;
     }
-    userScore.initials = initialsID.value;
+    // can't seem to get 'initials' to store...
+    userScore.initials =  initialsID.toString();
     
     var saveScores = localStorage.getItem("high scores");
     var scoreArr;
-
+    // If there are no saved scores, make an array
     if(saveScores === null)
     {
         scoreArr = [];
     }
-
+    // Else, parse the saved scores
     else
     {
         scoreArr = JSON.parse(saveScores);
     }
-
+    // Push userScore onto array, store in local storage
     scoreArr.push(userScore);
     localStorage.setItem("high scores", JSON.stringify(scoreArr));
 
     getHighScores();
 }
 
-// Gets high scores from local storage [View High Scores]
+// Gets high scores from local storage, 'View High Scores'
 function getHighScores(event)
 {
     toggleDisplay(showScores);
-    // needs to retrieve list of initials + scores
+    // Parses local storage key
     var getHS = JSON.parse(localStorage.getItem("high scores"));
 
+    // If not null, get values
     if(getHS !== null)
     {
-        initialsID.innerHTML = getHS.initials;
+        initialsID.value = getHS.initials;
         finalScore.innerHTML = getHS.score;
 
+    }
+        // Fill in ordered list with elements from userScore [initials and score]
         for (var i = 0; i < getHS.length; i++)
         {
             var newScore = document.createElement("ol");
-            newScore.innerHTML = getHS[i].initialsID + ": " + getHS[i].score;
+            newScore.innerHTML = getHS[i].initials + ": " + getHS[i].score;
             showScores.appendChild(newScore);
         }
-    }
+    
 
 }
 
@@ -239,10 +259,9 @@ function getHighScores(event)
 function toggleDisplay(x)
 {
     x.classList.toggle("hidden");
-
 }
 
-// Click "start" to start the quiz
+// Click "Start" to start the quiz
 startButton.addEventListener("click", startQuiz);
 
 // Selecting an answer
@@ -256,11 +275,15 @@ showScores.addEventListener("click", function(event)
 {
     getHighScores(event);
 });
+// ^Could not get it figured out :/
 
 // Click on "Go back" to return to previous screen
-goBackButton.addEventListener("click", function(event)
+goBackButton.addEventListener("click", function()
 {
-    toggleDisplay(event);
+    // Resets questions + quiz
+    currQuestion = 0;
+    toggleDisplay(codingQuizMain);
+    toggleDisplay(answersCorrectWrong);
 });
 
 // Click on "Clear high scores" to clear high scores
